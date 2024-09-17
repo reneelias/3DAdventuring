a8a71bb94e194387a0f4edde4d0c2e3d24f64959
Rene Elias <reneelias03@gmail.com>
2024-09-17 10:40:52 -0700

    Add basic GravObj properties + GravityType and ObjectShape enums

--- Assets/Scripts/GravitationalObjects/GravitationalObject.cs
+++ Assets/Scripts/GravitationalObjects/GravitationalObject.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GravitationalObject : MonoBehaviour
 {
+    [Tooltip("Gravity")]
+    [field: SerializeField] public GravityType gravityType{ get; private set; } = GravityType.SinglePoint;
+    [field: SerializeField] public float gravityScale{ get; private set;} = 9.81f;
+    [SerializeField] private GameObject gravityPoint;
+    [Tooltip("Shape")]
+    [field: SerializeField] public ObjectShape objectShape{ get; private set; } = ObjectShape.Sphere;
+    [SerializeField] private Collider gravTriggerCollider;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,15 @@ public class GravitationalObject : MonoBehaviour
     {
         
     }
+}
+
+public enum GravityType{
+    SinglePoint,
+    SurfaceNormal
+}
+
+public enum ObjectShape{
+    Sphere,
+    Cuboid,
+    Plane
 }
